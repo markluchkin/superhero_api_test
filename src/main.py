66858c76afa3a6562b91c4b97780b240da45b4a3
markluchkin <markluckin7@gmail.com>
2026-06-17 00:15:31 +0300

    update main.py & reqts

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
 import requests
+from typing import Optional, Dict, Any
+
+from config import API_URL
 
 
-def get_highest_superhero(gender: str, work: bool):
+def get_all_superheroes():
+    pass
+
+
+def get_tallest_superhero(gender: str, has_work: bool):
     pass
