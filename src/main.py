b2be4be1dd30297e06caa60786e4f8b080917552
markluchkin <markluckin7@gmail.com>
2026-06-17 00:35:41 +0300

    add get_superhero_height func && update get_all_superheroes

--- src/main.py
+++ src/main.py
@@ -5,13 +5,16 @@ from config import API_URL
 
 
 def get_all_superheroes() -> list:
-    try:
-        response = requests.get(API_URL)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        raise Exception(f"Error: {e}")
+    response = requests.get(API_URL)
+    response.raise_for_status()
+    return response.json()
+    
+
+def get_superhero_height(superhero: Dict[str, Any]) -> int:
+    height = superhero["appearance"]["height"][1]
+    return int(height.replace(" cm", ""))
 
 
 def get_tallest_superhero(gender: str, has_work: bool) -> Optional[Dict[str, Any]]:
-    pass
+    superheroes = get_all_superheroes()
+    filtered_superheroes = []
