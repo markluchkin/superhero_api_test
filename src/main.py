d5e93c1f0f38e4dd94ae752fec0511c05a633620
markluchkin <markluckin7@gmail.com>
2026-06-17 00:21:12 +0300

    add get_all_superheroes func

--- src/main.py
+++ src/main.py
@@ -4,9 +4,14 @@ from typing import Optional, Dict, Any
 from config import API_URL
 
 
-def get_all_superheroes():
-    pass
+def get_all_superheroes() -> list:
+    try:
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        return response.json()
+    except requests.RequestException as e:
+        raise Exception(f"Error: {e}")
 
 
-def get_tallest_superhero(gender: str, has_work: bool):
+def get_tallest_superhero(gender: str, has_work: bool) -> Optional[Dict[str, Any]]:
     pass
